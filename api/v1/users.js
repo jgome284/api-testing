7b1f01ab681dbd89f3c23599fdef491143854c30
Jose M. Gomez <gomezjosem@live.com>
2024-04-25 02:14:34 +0000

    moved and renamed file

--- api/v1/users.js
+++ api/v1/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { initDB } = require('./db');
+const { initDB } = require('../../data/db');
 
 const app = express();
 app.use(bodyParser.json());
